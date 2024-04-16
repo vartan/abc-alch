f72d00e4646c7a4b1b96dd3728de955c0e0c0b82
Michael Vartan <michael.wayne.vartan@gmail.com>
2024-04-15 18:38:06 -0700

    Update alch child ids; these change when OSRS changes the spellbook.

--- src/main/java/com/vartan/abc/model/Spell.java
+++ src/main/java/com/vartan/abc/model/Spell.java
@@ -1,12 +1,12 @@
 package com.vartan.abc.model;
 
 public enum Spell {
-    LOW_LEVEL_ALCHEMY(31, 3, 19),
-    HIGH_LEVEL_ALCHEMY(65, 5, 40);
+    LOW_LEVEL_ALCHEMY(31, 3, 21),
+    HIGH_LEVEL_ALCHEMY(65, 5, 44);
 
     public final int xpGained;
     /**
-     * The number of game ticks afer casting until the spell cools down.
+     * The number of game ticks after casting until the spell cools down.
      */
     public final int cooldown;
     public final int widgetChildId;
