15fdb8b3338967bfb08983680dc1910addf26bbf
rpwh <me@rpwh.dev>
2026-04-18 04:26:24 +0100

    WidgetIDs update from runelite api

--- src/main/java/com/vartan/abc/model/Spell.java
+++ src/main/java/com/vartan/abc/model/Spell.java
@@ -1,19 +1,17 @@
 package com.vartan.abc.model;
 
 public enum Spell {
-    LOW_LEVEL_ALCHEMY(31, 3, 21),
-    HIGH_LEVEL_ALCHEMY(65, 5, 44);
+    LOW_LEVEL_ALCHEMY(31, 3),
+    HIGH_LEVEL_ALCHEMY(65, 5);
 
     public final int xpGained;
     /**
      * The number of game ticks after casting until the spell cools down.
      */
     public final int cooldown;
-    public final int widgetChildId;
 
-    Spell(int xpGained, int cooldown, int widgetChildId) {
+    Spell(int xpGained, int cooldown) {
         this.xpGained = xpGained;
         this.cooldown = cooldown;
-        this.widgetChildId = widgetChildId;
     }
 }
